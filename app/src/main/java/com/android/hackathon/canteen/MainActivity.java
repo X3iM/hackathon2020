7d646e3441e284d030a8736d9f304d089cdbbf2d
Oleksandr Hrakhov <oleksandr.hrakhov@student.tuke.sk>
2020-12-02 11:56:17 +0100

    Create a new MenuActivity create a RecyclerViewAdapter, worked on disign

--- app/src/main/java/com/android/hackathon/canteen/MainActivity.java
+++ app/src/main/java/com/android/hackathon/canteen/MainActivity.java
@@ -1,15 +1,23 @@
 package com.android.hackathon.canteen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.android.hackathon.canteen.activities.adapter.Food;
+import com.android.hackathon.canteen.activities.adapter.RecyclerViewAdapter;
 import com.android.hackathon.canteen.database.DatabaseController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    List<Food> lstFood;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
 
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         DatabaseController.INSTANCE.makeRequest();
     }
 }
---
+++ app/src/main/java/com/android/hackathon/canteen/activities/adapter/RecyclerViewAdapter.java
@@ -0,0 +1,64 @@
+package com.android.hackathon.canteen.activities.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.hackathon.canteen.R;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<Food> mData;
+
+    public RecyclerViewAdapter(Context mContext, List<Food> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        LayoutInflater mInflate = LayoutInflater.from(mContext);
+        view = mInflate.inflate(R.layout.menu_item_dish,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.tv_food_title.setText(mData.get(position).getName());
+        holder.img_food_thumbnail.setImageResource(mData.get(position).getThumbnail());
+        holder.tv_food_price.setText(mData.get(position).getPrice());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView tv_food_title;
+        ImageView img_food_thumbnail;
+        TextView tv_food_price;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+
+            tv_food_title = (TextView)itemView.findViewById(R.id.food_title_id);
+            img_food_thumbnail = (ImageView)itemView.findViewById(R.id.food_img_id);
+            tv_food_price=(TextView)itemView.findViewById(R.id.food_title_price);
+        }
+    }
+}
