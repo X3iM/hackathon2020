fa1c2336391a90efdd33fb32a480732e58abc846
Artem Hrynenko <artem.hrynenko@student.tuke.sk>
2020-12-04 00:11:12 +0100

    none

--- app/src/main/java/com/android/hackathon/canteen/activities/MenuActivity.java
+++ app/src/main/java/com/android/hackathon/canteen/activities/MenuActivity.java
@@ -100,16 +100,24 @@ public class MenuActivity extends AppCompatActivity {
 
 //        foodList1.clear();
         hashMap.put("1", foodList1);
+        hashMap.put("3", foodList1);
+        hashMap.put("5", foodList1);
+        hashMap.put("7", foodList1);
+        hashMap.put("9", foodList1);
 
-//        foodList1 = new ArrayList<>();
-//        foodList1.add(new Food("1", "The Vegetarian", "https://firebasestorage.googleapis.com/v0/b/myunicanteen-2015b.appspot.com/o/chorizo.jpg?alt=media&token=0d64d905-8647-46ca-8a90-5d60ebc03954", "Vegetarian cuisine is based on food that meets vegetarian standards by not including meat and animal tissue products", "$2.00", "25", 194, "30", "30"));
-//        foodList1.add(new Food("2", "Cumin soup", "https://img.sndimg.com/food/image/upload/w_560,h_315,c_thumb,fl_progressive,q_80/v1/img/recipes/32/24/65/picnDxFgl.jpg", "This carrot and cumin soup recipe, based on a traditional Middle Eastern soup, has become a favorite with everyone who tastes it. It's also unbelievably healthy (almost no fat) and easy (few ingredients). The finished soup has a wonderful creamy texture, especially considering there is no cream or cheese in it.", "$2.75", "35", 295, "30", "25"));
-//        foodList1.add(new Food("3", "Creamy Sweet Potato", "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F8760746.jpg&q=85", "Loaded with sweet potato and warm spices, this oatmeal is a nutritious and tasty breakfast! It's perfect for using up leftover sweet potatoes. Just adjust the sweetener and butter if your recipe already has them. Top with toasted pecans, walnuts, or coconut.", "$1.90", "30", 341, "30", "40"));
-//        foodList1.add(new Food("4", "Vegetable curry", "https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/chickpea_spinach_and_egg_50755_16x9.jpg", "This vegetarian low-calorie chickpea curry makes a wonderful, easy after-work meal, and is ready in less than 20 minutes. It's also vegan if you leave out the egg.", "$2", "26", 441, "29", "42"));
-//        foodList1.add(new Food("5", "Salmon", "https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/salmon_broccoli_pasta_39926_16x9.jpg", "A simple salmon pasta that’s ready in under 15 minutes. This recipe makes two generous servings or three lighter meals. It’s also very easy to double up", "$3.90", "42", 549, "30", "25"));
-//        foodList1.add(new Food("6", "Adaptable bean", "https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/nigelsadaptablebeans_92230_16x9.jpg", "Nigel Slater's budget-friendly bean soup is healthy and filling. Chop and change the ingredients for your favourite vegetables and beans.", "$2.50", "43", 439, "35", "20"));
+        foodList1 = new ArrayList<>();
+        foodList1.add(new Food("1", "Ackee and saltfish", "https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/ackee_and_saltfish_with_73421_16x9.jpg", "Ackee and saltfish is a spicy Caribbean classic and makes a quick but flavourful meal.", "$3.25", "25", 194, "30", "30"));
+        foodList1.add(new Food("2", "Alpine pizza", "https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/alpine_pizza_32132_16x9.jpg", "Nothing beats making your own pizza dough. Here it’s topped with gruyère and salami for a delicious Alpine taste.", "$3.95", "35", 295, "30", "25"));
+        foodList1.add(new Food("3", "American style pancakes with sausage", "https://upload.wikimedia.org/wikipedia/commons/c/c5/Dennysbreakfast.jpg", "Loaded with sweet potato and warm spices, this oatmeal is a nutritious and tasty breakfast! It's perfect for using up leftover sweet potatoes. Just adjust the sweetener and butter if your recipe already has them. Top with toasted pecans, walnuts, or coconut.", "$1.90", "30", 341, "30", "40"));
+        foodList1.add(new Food("4", "Vegetable curry", "https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/chickpea_spinach_and_egg_50755_16x9.jpg", "This vegetarian low-calorie chickpea curry makes a wonderful, easy after-work meal, and is ready in less than 20 minutes. It's also vegan if you leave out the egg.", "$2", "26", 441, "29", "42"));
+        foodList1.add(new Food("5", "Aromatic beef curry", "https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/aromatic_beef_curry_90850_16x9.jpg", "Try this comforting curry, packed with flavour and tender beef. ", "$3.90", "42", 549, "30", "25"));
+        foodList1.add(new Food("6", "Artichoke pasta", "https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/artichokepasta_1398_16x9.jpg", "A delightfully simple recipe, ideal for a weeknight supper. Use fresh herbs to get the best flavours and complement the artichokes.", "$3.10", "43", 439, "35", "20"));
 
         hashMap.put("2", foodList1);
+        hashMap.put("4", foodList1);
+        hashMap.put("6", foodList1);
+        hashMap.put("8", foodList1);
+        hashMap.put("10", foodList1);
     }
 
 }
