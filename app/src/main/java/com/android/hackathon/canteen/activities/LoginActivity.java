b3e0cb0854e2d68295145dd091a6f4c634914f24
Alona <alona.rozlach@student.tuke.sk>
2020-12-03 11:53:01 +0200

    registration and authorization with firebase

---
+++ app/src/main/java/com/android/hackathon/canteen/activities/LoginActivity.java
@@ -0,0 +1,107 @@
+
+package com.android.hackathon.canteen.activities;
+import com.android.hackathon.canteen.MainActivity;
+import com.android.hackathon.canteen.R;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private EditText emails;
+    private EditText passwords;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+
+                } else {
+                    // User is signed out
+
+                }
+
+            }
+        };
+
+        emails = (EditText) findViewById(R.id.email);
+        passwords = (EditText) findViewById(R.id.password);
+
+       findViewById(R.id.login_btn).setOnClickListener(this);
+        findViewById(R.id.registr_btn).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.login_btn)
+        {
+            signin(emails.getText().toString(), passwords.getText().toString());
+        }else
+
+       if (view.getId() == R.id.registr_btn)
+        {
+            registration(emails.getText().toString(), passwords.getText().toString());
+        }
+
+    }
+
+    public void signin(String email , String password)
+    {
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "Authorization successful", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }else
+                    Toast.makeText(LoginActivity.this, "Authorization failed", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+    public void registration (String email , String password){
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(LoginActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+                else
+                    Toast.makeText(LoginActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
+
+
+
